cb64a4279d21fbc31bd94e5d7c3c2bf93897ff79
David Arenas <darensr@gmail.com>
2019-02-11 12:14:31 +0100

    fix(utils): do not use date_gmt in `normalizeComments`

--- src/pwa/utils/index.js
+++ src/pwa/utils/index.js
@@ -1,12 +1,5 @@
 /* eslint-disable import/prefer-default-export */
-
-const dateGmtRegex = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})$/;
-const normalizeDate = dateGmt => {
-  const [, year, month, day, hours, minutes, seconds] = dateGmt.match(
-    dateGmtRegex,
-  );
-  return new Date(Date.UTC(year, month - 1, day, hours, minutes, seconds));
-};
+const normalizeDate = date => new Date(date);
 
 export const normalizeComment = ({
   id,
@@ -15,7 +8,6 @@ export const normalizeComment = ({
   author_name: name,
   author_avatar_urls: { '96': avatar },
   date,
-  date_gmt: dateGmt,
   content: { rendered: content },
 }) => ({
   id: `${id}`,
@@ -24,5 +16,5 @@ export const normalizeComment = ({
   name,
   content,
   avatar,
-  date: normalizeDate(dateGmt || date),
+  date: normalizeDate(date),
 });
