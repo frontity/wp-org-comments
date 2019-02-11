b3e5ade1f8a0f7e66a3bd2ffe7071afe385395fd
David Arenas <darensr@gmail.com>
2019-02-11 20:38:35 +0100

    fix(components): use date view in CommentsList

--- src/pwa/components/WpOrgComments/CommentsList.js
+++ src/pwa/components/WpOrgComments/CommentsList.js
@@ -3,7 +3,13 @@ import Comment from './Comment';
 
 const CommentsList = ({ comments, onReply }) =>
   comments.map(c => (
-    <Comment key={c.id} replies={c.replies} {...c} onReply={onReply} />
+    <Comment
+      key={c.id}
+      replies={c.replies}
+      date={c.date}
+      {...c}
+      onReply={onReply}
+    />
   ));
 
 export default CommentsList;
