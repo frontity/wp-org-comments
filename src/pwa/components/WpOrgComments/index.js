2593c8a4dfcfb5fc13a041a80a9b0eb1182e44cd
David Arenas <darensr@gmail.com>
2019-02-11 12:27:22 +0100

    fix(styles): fix some margins

--- src/pwa/components/WpOrgComments/index.js
+++ src/pwa/components/WpOrgComments/index.js
@@ -150,8 +150,10 @@ const FormTitle = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  margin-top: 32px;
   h3 {
     font-weight: bold;
+    margin: 0;
   }
 `;
 
